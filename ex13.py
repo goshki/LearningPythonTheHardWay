55acf423e52ec92016b5b9e46bd18cb544063ff8
Adrian K <goshki@gmail.com>
2011-07-20 19:43:54 +0200

    Exercise 13.

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+# This script must be called with exactly three parameters
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
